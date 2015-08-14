73e46421b019d1a348edad69172e8536c6c18ed5
mgoodnow <michael.goodnow@bazaarvoice.com>
2015-08-14 14:23:19 -0500

    Fixing Java 8 Javadoc warnings and errors

--- mongodao/src/main/java/com/bazaarvoice/commons/data/dao/mongo/dbo/PipelineOperatorMongoDBObject.java
+++ mongodao/src/main/java/com/bazaarvoice/commons/data/dao/mongo/dbo/PipelineOperatorMongoDBObject.java
@@ -11,8 +11,7 @@ import java.util.Map;
 
 /**
  * Used to query MongoDB via the Aggregate Pipeline in a fluent style.
- *
- * <p/>
+ * <p>
  * <pre>
  * {@code
  * List<DBObject> pipeline = pipelineOf(
